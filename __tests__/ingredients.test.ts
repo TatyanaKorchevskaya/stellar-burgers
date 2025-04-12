8f88376ee76a851e7595f54f9262fbba364fe793
TatyanaKorchevskaya <tvkorchevskaya@gmail.com>
2025-04-12 14:45:34 +0300

    finish jest

--- __tests__/ingredients.test.ts
+++ __tests__/ingredients.test.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { store, rootReducer } from '../src/services/store';
-import { mockStore } from './data/mockStore';
-import { addIngredient, selectConstructorItems } from '../src/slices/stellarBurgerSlice';
-import { mockIngredient } from './data/mockIngredient';
+import { mockStore } from '../mock/mockStore';
+import { addIngredient, deleteIngredient, moveIngredientUp, selectConstructorItems } from '../src/slices/stellarBurgerSlice';
+import { mockIngredientAdd } from '../mock/mockIngredientAdd';
+import { mockIngredientDelete } from '../mock/mockIngredientDelete';
+import { mockIngredientMoveUp } from '../mock/mockIngredientMoveUp';
 
 const initStore = () => configureStore({
     reducer: rootReducer,
@@ -11,12 +13,33 @@ const initStore = () => configureStore({
     }
 });
 
-describe('Тестирование ingredients', () => {
-    test('Add ingredient', () => {
+describe('Testing ingredients', () => {
+
+    test('Delete ingredient', () => {
         const store = initStore();
-        store.dispatch(addIngredient(mockIngredient));
+        // console.log(selectConstructorItems(store.getState()).ingredients.length);
+        
+        store.dispatch(deleteIngredient(mockIngredientDelete));
 
         const res = selectConstructorItems(store.getState());
-        expect(res.ingredients.length).toEqual(4);
+        // console.log(selectConstructorItems(store.getState()).ingredients.length);
+        expect(res.ingredients.length).toBe(2);
+    })
+    test('Add ingredient', () => {
+        const store = initStore();
+        store.dispatch(addIngredient(mockIngredientAdd));
+
+        const res = selectConstructorItems(store.getState());
+        expect(res.ingredients.length).toEqual(3);
     });
+
+    test('Move up ingredient', () => {
+        const store = initStore();
+        store.dispatch(moveIngredientUp(mockIngredientMoveUp));
+
+        const ingredients = selectConstructorItems(store.getState()).ingredients;
+        expect(ingredients[ingredients.length-2]).toEqual(mockIngredientMoveUp);
+    });
+
+
 });
--- mock/mockIngredientAdd.ts
+++ mock/mockIngredientAdd.ts
@@ -1,4 +1,4 @@
-export const mockIngredient = {
+export const mockIngredientAdd = {
     "_id": "643d69a5c3f7b9001cfa093c",
     "name": "Краторная булка N-200i",
     "type": "bun",
